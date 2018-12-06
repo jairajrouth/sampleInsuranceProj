f78c0561acf522755effeca20f7acd7d5cb8de54
Jairaj Routh <Jairaj.Routh@msg.group>
2018-12-06 14:29:03 +0100

    SideBar Needs Update

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -8,6 +8,12 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class SidebarComponent implements OnInit {
 
+  menuItems: Array<any> = [
+    {title: 'Home', top: "130px", hovered: false, sub: ['sub OnePointOne', 'sub 1.2', 'sub 1.3', 'sub 1.4']},
+    {title: 'Contact Us', top: "170px", hovered: false, sub: ['sub 2.1', 'sub 2.2', 'sub 2.3']},
+    {title: 'About Us', top: "200px", hovered: false, sub: ['sub 3.1', 'sub 3.2', 'sub 3.3', 'sub 3.4', 'sub 3.5']},
+  ];
+
  @ViewChild('sidebar') sidebar : MatSidenav;
 
   close() {
@@ -18,4 +24,13 @@ export class SidebarComponent implements OnInit {
   ngOnInit() {
   }
 
+  mouseEnter(i: number) {
+    console.log(i);
+    this.menuItems.forEach((v => v.hovered = false));
+    this.menuItems[i].hovered = true;
+  }
+
+  mouseLeave(i: number) {
+    this.menuItems.forEach((v => v.hovered = false));
+  }
 }
